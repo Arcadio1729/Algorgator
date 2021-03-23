47448b9bb535f0e34d6619c0d2cc45b157bb6682
Arcadio1729 <35628271+Arcadio1729@users.noreply.github.com>
2021-03-23 10:32:08 +0100

    Add files via upload

---
+++ Matrix_Norm.py
@@ -0,0 +1,44 @@
+import numpy as np
+
+class Matrix_Norm_P:
+    def __init__(self,p_value,steps):
+        self.__p_value=p_value
+        self.__digits_lst=[]
+        self.__steps=steps
+        self.__vectors_set=[]
+
+    def __get_component(self,n, digit_from, m):
+        digit_to = n - m + 1
+        if digit_from > digit_to:
+            return 0
+        if m == 1:
+            self.__digits_lst.append(float(n))
+            self.__vectors_set.append(np.array(self.__digits_lst) / self.__steps)
+            self.__digits_lst.pop(-1)
+            return 1
+        current_sum = 0
+        for first_digit in range(digit_from, digit_to):
+            self.__digits_lst.append(first_digit)
+            current_sum += self.__get_component(n - first_digit, first_digit, m - 1, self.__digits_lst)
+            self.__digits_lst.pop(-1)
+        return current_sum
+
+    def __p(self,n, m):
+        return self.__get_component(n, 1, m)
+
+    def get_p_norm(self,in_vector, p):
+        current_sum = 0
+        for v in in_vector:
+            current_sum += v ** p
+        return current_sum ** (1 / p)
+
+    def calculate_p_norm(self,A):
+        print(len(A[0,:]))
+        self.__p(self.__steps,len(A[0,:]))
+        norm_values=[]
+
+        W = np.matrix(np.array(self.__vectors_set) ** (1 / self.__p_value))
+        U = A * W.T
+        for u in U.T:
+            norm_values.append(self.get_p_norm(np.array(u)[0], self.__p_value))
+        return max(norm_values)
